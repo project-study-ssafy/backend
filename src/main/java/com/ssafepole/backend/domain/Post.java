e82afe0a9b54d6fba166e806a001bc4542f16f46
jongchan0109 <jongchan1009@naver.com>
2024-10-19 20:05:34 +0900

    feat: 사용자와 포스트 연관관계 설정 #3

--- src/main/java/com/ssafepole/backend/domain/Post.java
+++ src/main/java/com/ssafepole/backend/domain/Post.java
@@ -1,5 +1,23 @@
 package com.ssafepole.backend.domain;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
 public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "post_id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
 
 }
