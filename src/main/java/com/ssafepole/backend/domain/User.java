e54e6673cdef22b84571841c577fcbae4e99e24b
jongchan0109 <jongchan1009@naver.com>
2024-10-19 20:10:24 +0900

    fix: ID Short로 수정 #3

--- src/main/java/com/ssafepole/backend/domain/User.java
+++ src/main/java/com/ssafepole/backend/domain/User.java
@@ -23,7 +23,7 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "user_id")
-    private Long id;
+    private Short id;
 
     private String username;
 
