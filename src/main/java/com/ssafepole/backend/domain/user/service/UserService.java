d8f5da9188fc81001fc5466d6a8297121b8bf8be
jongchan0109 <jongchan1009@naver.com>
2024-10-20 17:48:38 +0900

    feat: UserService 인터페이스 생성 #7

--- src/main/java/com/ssafepole/backend/domain/user/service/UserService.java
+++ src/main/java/com/ssafepole/backend/domain/user/service/UserService.java
@@ -1,5 +1,13 @@
 package com.ssafepole.backend.domain.user.service;
 
+import com.ssafepole.backend.domain.user.domain.User;
+import com.ssafepole.backend.domain.user.presentation.dto.request.UserSignUpRequest;
+
 public interface UserService {
 
+    User completeSignUp(UserSignUpRequest signUpRequest);
+
+    void validateEmail(String email);
+
+    void validateSignUpRequest(UserSignUpRequest signUpRequest);
 }
