77fe9051fc6487e8f22b4f8d14c4af1e4ba13fc1
jongchan0109 <jongchan1009@naver.com>
2024-10-20 17:52:10 +0900

    feat: 중복 이메일 검증 기능 구현 #7

--- src/main/java/com/ssafepole/backend/domain/user/service/UserServiceImpl.java
+++ src/main/java/com/ssafepole/backend/domain/user/service/UserServiceImpl.java
@@ -1,5 +1,26 @@
 package com.ssafepole.backend.domain.user.service;
 
 public class UserServiceImpl {
+import com.ssafepole.backend.domain.user.domain.repository.UserRepository;
+import com.ssafepole.backend.global.exception.user.DuplicatedEmailException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+    @Override
+    public void validateEmail(String email) {
+        if (isEmailExists(email)) {
+            throw new DuplicatedEmailException("이메일이 이미 사용 중입니다.");
+        }
+    }
+
+    private boolean isEmailExists(String email) {
+        return userRepository.existsByEmail(email);
+    }
 }
