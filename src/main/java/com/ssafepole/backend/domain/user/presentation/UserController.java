c72f0fc4bbe83f6b6320fead5e3961773b2c738f
jongchan0109 <jongchan1009@naver.com>
2024-10-20 17:57:25 +0900

    feat: 회원가입 요청 API 구현 #7

--- src/main/java/com/ssafepole/backend/domain/user/presentation/UserController.java
+++ src/main/java/com/ssafepole/backend/domain/user/presentation/UserController.java
@@ -1,8 +1,10 @@
 package com.ssafepole.backend.domain.user.presentation;
 
+import com.ssafepole.backend.domain.user.presentation.dto.request.UserSignUpRequest;
 import com.ssafepole.backend.domain.user.service.EmailService;
 import com.ssafepole.backend.domain.user.service.UserService;
 import com.ssafepole.backend.global.exception.user.DuplicatedEmailException;
+import com.ssafepole.backend.global.exception.user.DuplicatedNicknameException;
 import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Email;
@@ -55,4 +57,28 @@ public class UserController {
         return ResponseEntity.ok("이메일 인증이 완료되었습니다.");
     }
 
+
+    @PostMapping("/sign-up")
+    @Operation(summary = "회원가입", description = "회원가입을 위한 API", tags = {"회원가입"})
+    public ResponseEntity<String> signUp(@Valid @RequestBody UserSignUpRequest signUpRequest) {
+
+        try {
+            userService.validateSignUpRequest(signUpRequest);
+        } catch (DuplicatedEmailException |DuplicatedNicknameException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+
+        // 이메일 인증 확인
+        if (!emailService.isEmailVerified(signUpRequest.getEmail())) {
+            return ResponseEntity.badRequest().body("이메일 인증이 필요합니다.");
+        }
+
+        // 사용자 정보 저장 (비밀번호 해싱 포함)
+        userService.completeSignUp(signUpRequest);
+
+        // 인증 코드 삭제
+        emailService.deleteEmailVerificationCode(signUpRequest.getEmail());
+
+        return ResponseEntity.ok("회원가입이 완료되었습니다.");
+    }
 }
