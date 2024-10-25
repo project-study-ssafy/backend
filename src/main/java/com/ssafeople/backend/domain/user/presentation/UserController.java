27b5211a0ce50e855dcdc9147ae9c8f80078aef9
jongchan0109 <jongchan1009@naver.com>
2024-10-25 16:49:33 +0900

    fix: 인증 코드 전송 코드 수정 #7

--- src/main/java/com/ssafeople/backend/domain/user/presentation/UserController.java
+++ src/main/java/com/ssafeople/backend/domain/user/presentation/UserController.java
@@ -3,7 +3,7 @@ package com.ssafeople.backend.domain.user.presentation;
 import com.ssafeople.backend.domain.auth.security.JwtUtil;
 import com.ssafeople.backend.domain.user.domain.User;
 import com.ssafeople.backend.domain.user.presentation.dto.request.UserSignUpRequest;
-import com.ssafeople.backend.domain.user.presentation.dto.response.UserSignUpResponse;
+import com.ssafeople.backend.domain.user.presentation.dto.request.UserVerifyCodeRequest;
 import com.ssafeople.backend.domain.user.service.EmailService;
 import com.ssafeople.backend.domain.user.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
@@ -11,6 +11,8 @@ import jakarta.validation.Valid;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,6 +21,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 @Validated
@@ -30,19 +33,13 @@ public class UserController {
 
     @PostMapping("/sign-up/send-verification-code")
     @Operation(summary = "인증 코드 전송", description = "회원가입 시 이메일로 인증 코드를 전송하는 API", tags = {"회원가입"})
-    public ResponseEntity<String> sendVerificationCode(
+    public void sendVerificationCode(
         @RequestParam @NotBlank(message = "이메일을 입력해주세요.") @Email(message = "유효한 이메일 형식이 아닙니다.") String email) {
 
-        // 이메일 중복 확인
-        try {
-            userService.validateEmail(email);
-        } catch (DuplicatedEmailException e) {
-            return ResponseEntity.badRequest().body(e.getMessage());
-        }
+        userService.validateEmail(email);
 
-        // 인증 코드 생성 및 전송
         emailService.sendVerificationCode(email);
-        return ResponseEntity.ok("인증 코드가 전송되었습니다.");
+        log.info("인증 코드 전송");
     }
 
     @PostMapping("/sign-up/verify-code")
