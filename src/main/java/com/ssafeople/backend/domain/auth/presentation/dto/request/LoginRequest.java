1210df12e160d552a8107fbf812bac5c7c2b1e5f
jongchan0109 <jongchan1009@naver.com>
2024-10-25 17:12:18 +0900

    feat: 로그인 요청시 전달되는 객체 DTO 생성 #9

---
+++ src/main/java/com/ssafeople/backend/domain/auth/presentation/dto/request/LoginRequest.java
@@ -0,0 +1,20 @@
+package com.ssafeople.backend.domain.auth.presentation.dto.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class LoginRequest {
+
+    @Email(message = "이메일 형식으로 입력해주세요.")
+    @NotBlank(message = "이메일을 입력해주세요.")
+    private String email;
+
+    @NotBlank(message = "비밀번호를 입력해주세요.")
+    @Size(min = 8, max = 16, message = "비밀번호는 최소8자에서 16자까지 가능합니다.")
+    private String password;
+}
