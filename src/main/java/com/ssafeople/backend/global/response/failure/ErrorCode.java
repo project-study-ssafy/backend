fe0d87ea7eb86b1d5771673a1d4cd631ea793a01
jongchan0109 <jongchan1009@naver.com>
2024-10-25 17:10:03 +0900

    feat: 사용할 예외 코드, 메세지 생성 #9

---
+++ src/main/java/com/ssafeople/backend/global/response/failure/ErrorCode.java
@@ -0,0 +1,26 @@
+package com.ssafeople.backend.global.response.failure;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum ErrorCode {
+
+    INVALID_VERIFICATION_CODE(400, "유효하지 않은 인증번호입니다."),
+    EMAIL_NOT_VERIFIED(400, "인증되지 않은 이메일입니다."),
+
+    INVALID_TOKEN(401, "토큰이 유효하지 않습니다."),
+    EXPIRED_TOKEN(401, "토큰이 만료되었습니다."),
+
+    URL_INPUT_ERROR(404, "잘못된 URL 입니다"),
+    METHOD_NOT_ALLOWED(405, "http 메소드가 잘못되었습니다."),
+
+    DUPLICATE_EMAIL(422, "이미 존재하는 이메일입니다."),
+    DUPLICATE_NICKNAME(422, "이미 존재하는 닉네입입니다."),
+
+    INTERNAL_SERVER_ERROR(500, "내부 서버 에러");
+
+    private final int status;
+    private final String reason;
+}
