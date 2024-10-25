96937dbbc1e41aacab738b789825888d74f3bf77
jongchan0109 <jongchan1009@naver.com>
2024-10-25 17:08:18 +0900

    feat: 시큐리티 환경 설정 #9

---
+++ src/main/java/com/ssafeople/backend/global/config/SecurityConfig.java
@@ -0,0 +1,67 @@
+package com.ssafeople.backend.global.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ssafeople.backend.domain.auth.security.JwtAuthenticationFilter;
+import com.ssafeople.backend.domain.auth.security.JwtFilter;
+import com.ssafeople.backend.domain.auth.security.JwtUtil;
+import com.ssafeople.backend.global.exception.ExceptionFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final AuthenticationConfiguration authenticationConfiguration;
+    private final JwtUtil jwtUtil;
+    private final ObjectMapper objectMapper;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(
+        AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http.csrf(AbstractHttpConfigurer::disable)
+            .formLogin(AbstractHttpConfigurer::disable)
+            .httpBasic(AbstractHttpConfigurer::disable)
+            .authorizeHttpRequests((auth) -> auth
+                .requestMatchers("/api/users/sign-up/**").permitAll()
+                .requestMatchers("/api/users/login").permitAll()
+                .requestMatchers("/api-docs/**", "/swagger-ui/**").permitAll()
+                .requestMatchers("/api/auth/admin/**").hasRole("ADMIN")
+                .anyRequest().authenticated())
+            .sessionManagement((session) -> session
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        http.addFilterAt(
+            new JwtAuthenticationFilter(authenticationManager(authenticationConfiguration),
+                objectMapper, jwtUtil), UsernamePasswordAuthenticationFilter.class);
+
+        http.addFilterBefore(new ExceptionFilter(objectMapper), JwtAuthenticationFilter.class);
+
+        http.addFilterBefore(new JwtFilter(jwtUtil), JwtAuthenticationFilter.class);
+
+        return http.build();
+    }
+}
