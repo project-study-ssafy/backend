faf86d931018477718d1e2faf132fff9d2a22fd3
jongchan0109 <jongchan1009@naver.com>
2024-10-25 16:47:05 +0900

    fix: 클래스 재 설정으로 인한 삭제 #5

--- src/main/java/com/ssafepole/backend/global/exception/user/DuplicatedNicknameException.java
+++
@@ -1,8 +0,0 @@
-package com.ssafepole.backend.global.exception.user;
-
-public class DuplicatedNicknameException extends RuntimeException {
-
-    public DuplicatedNicknameException(String message) {
-        super(message);
-    }
-}
