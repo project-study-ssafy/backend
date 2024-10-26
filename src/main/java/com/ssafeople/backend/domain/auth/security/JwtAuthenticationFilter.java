401de81519cac83a686c13f0d0c77a73f1506b45
jongchan0109 <jongchan1009@naver.com>
2024-10-26 16:36:38 +0900

    refactor: JSON 값 반환하도록 수정 #11

--- src/main/java/com/ssafeople/backend/domain/auth/security/JwtAuthenticationFilter.java
+++ src/main/java/com/ssafeople/backend/domain/auth/security/JwtAuthenticationFilter.java
@@ -76,6 +76,7 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
         jwtUtil.sendAccessToken(response, "Bearer " + token);
 
         SuccessResponse successResponse = new SuccessResponse(HttpStatus.OK.value(), null);
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
         response.getWriter().write(objectMapper.writeValueAsString(successResponse));
     }
 
