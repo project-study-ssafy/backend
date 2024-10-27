9ba420ddc4204e071f4791acf79832e15cfea224
jongchan0109 <jongchan1009@naver.com>
2024-10-27 21:25:11 +0900

    feat: 사용자 정보 반환 기능 구현 #13

--- src/main/java/com/ssafeople/backend/domain/user/service/UserService.java
+++ src/main/java/com/ssafeople/backend/domain/user/service/UserService.java
@@ -1,6 +1,7 @@
 package com.ssafeople.backend.domain.user.service;
 
 import com.ssafeople.backend.domain.user.domain.User;
+import com.ssafeople.backend.domain.user.domain.vo.UserInfoVo;
 import com.ssafeople.backend.domain.user.presentation.dto.request.UserSignUpRequest;
 
 public interface UserService {
@@ -10,4 +11,6 @@ public interface UserService {
     void validateEmail(String email);
 
     void validateSignUpRequest(UserSignUpRequest signUpRequest);
+
+    UserInfoVo getUserInfo(String email);
 }
--- src/main/java/com/ssafeople/backend/domain/user/service/UserServiceImpl.java
+++ src/main/java/com/ssafeople/backend/domain/user/service/UserServiceImpl.java
@@ -2,9 +2,11 @@ package com.ssafeople.backend.domain.user.service;
 
 import com.ssafeople.backend.domain.user.domain.User;
 import com.ssafeople.backend.domain.user.domain.repository.UserRepository;
+import com.ssafeople.backend.domain.user.domain.vo.UserInfoVo;
 import com.ssafeople.backend.domain.user.presentation.dto.request.UserSignUpRequest;
 import com.ssafeople.backend.global.exception.user.DuplicatedEmailException;
 import com.ssafeople.backend.global.exception.user.DuplicatedNicknameException;
+import com.ssafeople.backend.global.exception.user.UserNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -33,6 +35,7 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
+    @Transactional(readOnly = true)
     public void validateEmail(String email) {
         userRepository.findByEmail(email).ifPresent(user -> {
             throw DuplicatedEmailException.EXCEPTION;
@@ -40,6 +43,7 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
+    @Transactional(readOnly = true)
     public void validateSignUpRequest(UserSignUpRequest signUpRequest) {
 
         userRepository.findByEmail(signUpRequest.getEmail()).ifPresent(user -> {
@@ -49,5 +53,12 @@ public class UserServiceImpl implements UserService {
         userRepository.findByNickname(signUpRequest.getNickname()).ifPresent( user -> {
             throw DuplicatedNicknameException.EXCEPTION;
         });
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public UserInfoVo getUserInfo(String email) {
+        return userRepository.findByEmail(email)
+            .orElseThrow(() -> UserNotFoundException.EXCEPTION).getUserInfo();
     }
 }
