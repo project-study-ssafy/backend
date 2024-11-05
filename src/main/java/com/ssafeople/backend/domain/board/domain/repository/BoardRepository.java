c2d365f24210a26ec26b710cccb8628df63c8550
name <grandsword25@gmail.com>
2024-11-05 17:21:37 +0900

    feat: 게시판 서비스 구현 #31

--- src/main/java/com/ssafeople/backend/domain/board/domain/repository/BoardRepository.java
+++ src/main/java/com/ssafeople/backend/domain/board/domain/repository/BoardRepository.java
@@ -1,5 +1,7 @@
 package com.ssafeople.backend.domain.board.domain.repository;
 
-public interface BoardRepository {
+import com.ssafeople.backend.domain.board.domain.Board;
+import org.springframework.data.jpa.repository.JpaRepository;
 
+public interface BoardRepository extends JpaRepository<Board, Short> {
 }
--- src/main/java/com/ssafeople/backend/domain/board/service/BoardServiceImpl.java
+++ src/main/java/com/ssafeople/backend/domain/board/service/BoardServiceImpl.java
@@ -1,5 +1,38 @@
 package com.ssafeople.backend.domain.board.service;
 
-public class BoardServiceImpl {
+import com.ssafeople.backend.domain.board.domain.Board;
+import com.ssafeople.backend.domain.board.domain.repository.BoardRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Collections;
+import java.util.List;
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
+@Transactional
+public class BoardServiceImpl implements BoardService {
+
+    private final BoardRepository boardRepository;
+
+    @Transactional(readOnly = true)
+    public List<Board> getAllBoards() {
+        List<Board> boards = boardRepository.findAll();
+
+        if (boards.isEmpty()) {
+            return Collections.emptyList();
+        }
+
+        return boards;
+    }
+
+    @Transactional(readOnly = true)
+    public Board getBoardById(Short id) {
+        return boardRepository.findById(id).orElseThrow(() -> new RuntimeException());
+    }
+
 
 }
