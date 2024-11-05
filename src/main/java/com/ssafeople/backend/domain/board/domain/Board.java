d92cc001352fb37c5a147e430b29852ab9d7ee06
name <grandsword25@gmail.com>
2024-11-05 17:13:04 +0900

    feat: 게시판 도메인 구현 #29

--- src/main/java/com/ssafeople/backend/domain/board/domain/Board.java
+++ src/main/java/com/ssafeople/backend/domain/board/domain/Board.java
@@ -1,5 +1,40 @@
 package com.ssafeople.backend.domain.board.domain;
 
-public class Board {
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.Immutable;
+import org.hibernate.annotations.UpdateTimestamp;
 
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "board")
+@Getter
+@NoArgsConstructor
+public class Board {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "board_id")
+    private Short id;
+
+    @Column(nullable = false, unique = true)
+    private String boardName;
+
+    private String description;
+
+    @Column(name = "created_at")
+    @CreationTimestamp
+    @Immutable
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+    public void update(String newBoardName, String newDescription) {
+        this.boardName = newBoardName;
+        this.description = newDescription;
+    }
 }
