bce80ef71b57a9a2735b04fad310bc64a00dbb72
name <grandsword25@gmail.com>
2024-11-07 08:59:24 +0900

    feat: 게시글 도메인 작성 #39

--- src/main/java/com/ssafeople/backend/domain/comment/domain/Comment.java
+++ src/main/java/com/ssafeople/backend/domain/comment/domain/Comment.java
@@ -1,5 +1,18 @@
 package com.ssafeople.backend.domain.comment.domain;
 
+import com.ssafeople.backend.domain.post.domain.Post;
+import jakarta.persistence.*;
+
+@Entity
 public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "comment_id")
+    private String id;
+
+    @ManyToOne
+    @JoinColumn(name = "post_id")
+    private Post post;
 
 }
--- src/main/java/com/ssafeople/backend/domain/post/domain/Post.java
+++ src/main/java/com/ssafeople/backend/domain/post/domain/Post.java
@@ -1,15 +1,18 @@
 package com.ssafeople.backend.domain.post.domain;
 
+import com.ssafeople.backend.domain.comment.domain.Comment;
 import com.ssafeople.backend.domain.user.domain.User;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.Immutable;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
+@Table(name = "posts")
 public class Post {
 
     @Id
@@ -21,4 +24,26 @@ public class Post {
     @JoinColumn(name = "user_id")
     private User user;
 
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "content")
+    private String content;
+
+    @Column(name = "created_at")
+    @CreationTimestamp
+    @Immutable
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+    @OneToMany(mappedBy = "posts", cascade = CascadeType.ALL)
+    private final List<Comment> comments = new ArrayList<>();
+
+    public void update(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
 }
