b86073c3f015377ea0ef995ae1e7019fa600bc11
name <grandsword25@gmail.com>
2024-11-06 22:24:19 +0900

    feat: 게시판 테스트 작성 #33

--- src/test/java/com/ssafeople/backend/domain/board/service/BoardServiceImplTest.java
+++ src/test/java/com/ssafeople/backend/domain/board/service/BoardServiceImplTest.java
@@ -13,7 +13,6 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SpringBootTest
