3119a531f493624d486047ba10ec72f568828f0b
name <grandsword25@gmail.com>
2024-11-07 09:09:15 +0900

    feat: 연관 관계 설정을 위한 댓글 도메인 작성 #39

--- src/main/java/com/ssafeople/backend/domain/comment/domain/Comment.java
+++ src/main/java/com/ssafeople/backend/domain/comment/domain/Comment.java
@@ -1,5 +1,14 @@
 package com.ssafeople.backend.domain.comment.domain;
 
+import com.ssafeople.backend.domain.post.domain.Post;
+import jakarta.persistence.*;
+
+@Entity
 public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "comment_id")
+    private String id;
 
 }
