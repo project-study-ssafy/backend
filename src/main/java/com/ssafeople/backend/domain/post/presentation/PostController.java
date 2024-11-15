086f4f59a96deda95ae1a3032b3e1d05f06709f0
name <grandsword25@gmail.com>
2024-11-15 09:43:56 +0900

    feat: 게시글 목록 조회 API 구현 #43

--- src/main/java/com/ssafeople/backend/domain/post/presentation/PostController.java
+++ src/main/java/com/ssafeople/backend/domain/post/presentation/PostController.java
@@ -1,5 +1,34 @@
 package com.ssafeople.backend.domain.post.presentation;
 
+import com.ssafeople.backend.domain.post.domain.Post;
+import com.ssafeople.backend.domain.post.service.PostService;
+import com.ssafeople.backend.global.exception.post.PostListEmptyException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@Slf4j
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/posts")
 public class PostController {
+    private final PostService postService;
+
+    @GetMapping("/post-list")
+    public ResponseEntity<List<Post>> getPostsByBoard(@RequestParam(required = true) Short boardId) {
+        List<Post> posts = null;
+        try {
+            posts = postService.getPostsByBoardId(boardId);
+        } catch (PostListEmptyException e) {
+            log.error("이 게시판은 글이 없네");
+        }
+        return ResponseEntity.ok(posts);
+    }
 
 }
