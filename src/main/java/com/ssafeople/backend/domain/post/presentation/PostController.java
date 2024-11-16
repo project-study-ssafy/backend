bae65da0794170c21370043a9cdffc69f457792f
name <grandsword25@gmail.com>
2024-11-16 15:34:46 +0900

    feat: 게시글 목록 조회 API 구현 #43

--- src/main/java/com/ssafeople/backend/domain/post/presentation/PostController.java
+++ src/main/java/com/ssafeople/backend/domain/post/presentation/PostController.java
@@ -1,34 +1,23 @@
 package com.ssafeople.backend.domain.post.presentation;
 
-import com.ssafeople.backend.domain.post.domain.Post;
+import com.ssafeople.backend.domain.post.domain.vo.PostInfoVO;
 import com.ssafeople.backend.domain.post.service.PostService;
-import com.ssafeople.backend.global.exception.post.PostListEmptyException;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+import java.util.*;
 
 @RestController
-@Slf4j
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/posts")
+@RequestMapping("/api/v1/boards/{boardId}/posts")
 public class PostController {
+
     private final PostService postService;
 
-    @GetMapping("/post-list")
-    public ResponseEntity<List<Post>> getPostsByBoard(@RequestParam(required = true) Short boardId) {
-        List<Post> posts = null;
-        try {
-            posts = postService.getPostsByBoardId(boardId);
-        } catch (PostListEmptyException e) {
-            log.error("이 게시판은 글이 없네");
-        }
+    @GetMapping
+    public ResponseEntity<List<PostInfoVO>> getPostsByBoard(@PathVariable(name="boardId") Short boardId) {
+        List<PostInfoVO> posts = postService.getPostsByBoardId(boardId);
         return ResponseEntity.ok(posts);
     }
-
 }
