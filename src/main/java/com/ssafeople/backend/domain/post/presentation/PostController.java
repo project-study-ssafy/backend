24ba9e07c93586dc29c0a83ad7391e0fe34b7763
name <grandsword25@gmail.com>
2024-11-18 13:19:09 +0900

    feat: 게시글 작성 API 구현 #45

--- src/main/java/com/ssafeople/backend/domain/post/presentation/PostController.java
+++ src/main/java/com/ssafeople/backend/domain/post/presentation/PostController.java
@@ -1,9 +1,15 @@
 package com.ssafeople.backend.domain.post.presentation;
 
+import com.ssafeople.backend.domain.post.presentation.dto.request.PostWriteRequest;
 import com.ssafeople.backend.domain.post.presentation.dto.response.PostSummaryResponse;
 import com.ssafeople.backend.domain.post.service.PostService;
+import com.ssafeople.backend.domain.user.domain.User;
+import com.ssafeople.backend.domain.user.service.UserService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
@@ -14,10 +20,21 @@ import java.util.*;
 public class PostController {
 
     private final PostService postService;
+    private final UserService userService;
 
     @GetMapping
     public ResponseEntity<List<PostSummaryResponse>> getPostsByBoard(@PathVariable(name="boardId") Short boardId) {
         List<PostSummaryResponse> posts = postService.getPostsByBoardId(boardId);
         return ResponseEntity.ok(posts);
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> writePost(
+            @PathVariable(name="boardId") Short boardId,
+            @Valid @RequestBody PostWriteRequest request, @AuthenticationPrincipal String email
+    ) {
+        User user = userService.getUser(email);
+        postService.writePost(request, boardId, user);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 }
