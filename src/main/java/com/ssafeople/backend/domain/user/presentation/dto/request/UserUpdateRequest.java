d2ee83f3b00ff90a34f0d3eb48a470f3050eba19
jongchan0109 <jongchan1009@naver.com>
2024-12-06 14:26:02 +0900

    fix: dto 수정

--- src/main/java/com/ssafeople/backend/domain/user/presentation/dto/request/UserUpdateRequest.java
+++ src/main/java/com/ssafeople/backend/domain/user/presentation/dto/request/UserUpdateRequest.java
@@ -1,6 +1,5 @@
 package com.ssafeople.backend.domain.user.presentation.dto.request;
 
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
@@ -8,12 +7,10 @@ import lombok.Setter;
 @Getter
 @Setter
 public class UserUpdateRequest {
-    @NotBlank(message = "이름을 입력해주세요.")
-    @Size(max = 12, message = "이름은 최대 12글자까지 가능합니다.")
+    @Size(min = 2, max = 12, message = "이름은 2 ~ 12글자까지 가능합니다.")
     private String username;
 
-    @NotBlank(message = "별명을 입력해주세요.")
-    @Size(max = 20, message = "별명은 20글자까지 가능합니다.")
+    @Size(min = 2, max = 20, message = "별명은 2 ~ 20글자까지 가능합니다.")
     private String nickname;
 
     @Size(max = 255, message = "한줄 소개는 255자 까지 가능합니다.")
