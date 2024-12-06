9dba7b42367ba5439dddc31b92047136a80ac729
jongchan0109 <jongchan1009@naver.com>
2024-12-06 14:04:29 +0900

    fix: 회원가입, 회원 정보 수정 dto 수정

--- src/main/java/com/ssafeople/backend/domain/user/presentation/dto/request/UserSignUpRequest.java
+++ src/main/java/com/ssafeople/backend/domain/user/presentation/dto/request/UserSignUpRequest.java
@@ -1,7 +1,6 @@
 package com.ssafeople.backend.domain.user.presentation.dto.request;
 
 import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
@@ -11,22 +10,17 @@ import lombok.Setter;
 @Setter
 public class UserSignUpRequest {
 
-    @NotBlank(message = "이름을 입력해주세요.")
-    @Size(max = 12, message = "이름은 최대 12글자까지 가능합니다.")
+    @Size(min = 2, max = 12, message = "이름은 2 ~ 12글자까지 가능합니다.")
     private String username;
 
     @Email(message = "이메일 형식으로 입력해주세요.")
-    @NotBlank(message = "이메일을 입력해주세요.")
     private String email;
 
-    @NotBlank(message = "비밀번호를 입력해주세요.")
-    @Size(min = 8, max = 16, message = "비밀번호는 최소8자에서 16자까지 가능합니다.")
     @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#$%^&*]).{8,16}$",
         message = "비밀번호는 숫자, 문자, 특수문자를 포함해야 합니다.")
     private String password;
 
-    @NotBlank(message = "별명을 입력해주세요.")
-    @Size(max = 20, message = "별명은 20글자까지 가능합니다.")
+    @Size(min = 2, max = 20, message = "별명은 2 ~ 20글자까지 가능합니다.")
     private String nickname;
 
 }
