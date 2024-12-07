1f0045ebc48cc341bdf8782270f38f3d0ba0cf88
name <grandsword25@gmail.com>
2024-12-07 23:05:27 +0900

    fix: 게시글 수정 Dto 수정 #52

--- src/main/java/com/ssafeople/backend/domain/post/presentation/dto/request/PostUpdateRequest.java
+++ src/main/java/com/ssafeople/backend/domain/post/presentation/dto/request/PostUpdateRequest.java
@@ -1,6 +1,5 @@
 package com.ssafeople.backend.domain.post.presentation.dto.request;
 
-import com.ssafeople.backend.domain.user.domain.User;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
@@ -8,14 +7,9 @@ import lombok.Setter;
 @Getter
 @Setter
 public class PostUpdateRequest {
-    private Short boardId;
-    private Long postId;
-
     @NotBlank(message = "제목은 필수 입력 항목입니다.")
     private String title;
 
     @NotBlank(message = "내용은 빈 칸일 수 없습니다. 내용을 입력해주세요.")
     private String content;
-
-    private User user;
 }
