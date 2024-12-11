13003bc7a29d4651f7a84a4893ea9f9768bf7ce5
name <grandsword25@gmail.com>
2024-12-11 16:56:12 +0900

    feat: 게시글 상세정보 조회 DTO 구현 #61

---
+++ src/main/java/com/ssafeople/backend/domain/post/presentation/dto/response/PostDetailResponse.java
@@ -0,0 +1,18 @@
+package com.ssafeople.backend.domain.post.presentation.dto.response;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
+public class PostDetailResponse {
+    private final Long id;
+    private final String title;
+    private final String content;
+    private final LocalDateTime createdAt;
+
+    private final Short userId;
+    private final String userName;
+}
