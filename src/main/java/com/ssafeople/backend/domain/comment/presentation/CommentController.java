ce6acc2448746cf662b28f71d04fa15668f2ddb4
name <grandsword25@gmail.com>
2024-12-13 17:10:15 +0900

    feat: 댓글 목록 조회 API 구현 #69

--- src/main/java/com/ssafeople/backend/domain/comment/presentation/CommentController.java
+++ src/main/java/com/ssafeople/backend/domain/comment/presentation/CommentController.java
@@ -1,5 +1,27 @@
 package com.ssafeople.backend.domain.comment.presentation;
 
+import com.ssafeople.backend.domain.comment.presentation.dto.response.CommentResponse;
+import com.ssafeople.backend.domain.comment.service.CommentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/boards/{boardId}/posts/{postId}/comments")
 public class CommentController {
 
+    private final CommentService commentService;
+
+    @GetMapping
+    public ResponseEntity<Page<CommentResponse>> getPageCommentsList(
+            @PathVariable Short boardId,
+            @PathVariable Long postId,
+            @RequestParam(required = false, defaultValue = "1", value = "page") int page,
+            @RequestParam(required = false, defaultValue = "10", value = "size") int size
+    ) {
+        Page<CommentResponse> pagedComments = commentService.getCommentListByPostId(boardId, postId, page, size);
+        return ResponseEntity.ok(pagedComments);
+    }
 }
