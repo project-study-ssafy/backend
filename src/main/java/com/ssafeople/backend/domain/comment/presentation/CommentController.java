d8f8013d99df420136de88f359ce7c5906c38547
name <grandsword25@gmail.com>
2024-12-13 20:33:21 +0900

    feat: 댓글 작성 API 구현 #71

--- src/main/java/com/ssafeople/backend/domain/comment/presentation/CommentController.java
+++ src/main/java/com/ssafeople/backend/domain/comment/presentation/CommentController.java
@@ -1,9 +1,14 @@
 package com.ssafeople.backend.domain.comment.presentation;
 
+import com.ssafeople.backend.domain.comment.presentation.dto.request.CommentWriteRequest;
 import com.ssafeople.backend.domain.comment.presentation.dto.response.CommentResponse;
 import com.ssafeople.backend.domain.comment.service.CommentService;
+import com.ssafeople.backend.domain.user.domain.User;
+import com.ssafeople.backend.global.utils.user.UserUtils;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,6 +18,7 @@ import org.springframework.web.bind.annotation.*;
 public class CommentController {
 
     private final CommentService commentService;
+    private final UserUtils userUtils;
 
     @GetMapping
     public ResponseEntity<Page<CommentResponse>> getPageCommentsList(
@@ -22,5 +28,15 @@ public class CommentController {
     ) {
         Page<CommentResponse> pagedComments = commentService.getCommentListByPostId(postId, page, size);
         return ResponseEntity.ok(pagedComments);
+    }
+
+    @PostMapping
+    public ResponseEntity<CommentResponse> writeComment(
+            @PathVariable Long postId,
+            @Valid @RequestBody CommentWriteRequest request
+    ) {
+        User user = userUtils.getCurrentUser();
+        commentService.writeComment(request, user, postId);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 }
