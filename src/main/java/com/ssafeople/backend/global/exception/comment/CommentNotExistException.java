763f1b2cd25719889f32aaa503272bac72dc5ab0
name <grandsword25@gmail.com>
2024-12-13 21:25:11 +0900

    feat: 댓글 삭제 예외 구현 #73

---
+++ src/main/java/com/ssafeople/backend/global/exception/comment/CommentNotExistException.java
@@ -0,0 +1,10 @@
+package com.ssafeople.backend.global.exception.comment;
+
+import com.ssafeople.backend.global.exception.SsafeopleException;
+import com.ssafeople.backend.global.response.failure.ErrorCode;
+
+public class CommentNotExistException extends SsafeopleException {
+    public static final CommentNotExistException EXCEPTION = new CommentNotExistException();
+
+    private CommentNotExistException() { super(ErrorCode.INVALID_COMMENT_ID); }
+}
