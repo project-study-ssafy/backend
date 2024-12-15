70f7a09613a1d200b2efc8ce2d04ae9437f75fc6
jongchan0109 <jongchan1009@naver.com>
2024-12-15 14:45:16 +0900

    feat: 메세지 응답 DTO 구현 #58

---
+++ src/main/java/com/ssafeople/backend/domain/chatting/presentation/dto/response/ChattingResponse.java
@@ -0,0 +1,22 @@
+package com.ssafeople.backend.domain.chatting.presentation.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import java.time.LocalDateTime;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+public class ChattingResponse{
+
+    private String nickname;
+
+    private String content;
+
+    private String sessionId;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime createdAt;
+}
