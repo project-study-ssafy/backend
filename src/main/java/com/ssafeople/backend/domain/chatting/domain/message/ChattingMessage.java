4b6b42a17841cbcb7bcab10e2edb30530513cdae
jongchan0109 <jongchan1009@naver.com>
2024-12-15 14:38:41 +0900

    feat: 채팅 메세지 도메인 구현 #58

---
+++ src/main/java/com/ssafeople/backend/domain/chatting/domain/message/ChattingMessage.java
@@ -0,0 +1,42 @@
+package com.ssafeople.backend.domain.chatting.domain.message;
+
+import com.ssafeople.backend.domain.chatting.domain.room.ChattingRoom;
+import com.ssafeople.backend.global.database.BaseEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "chatting_messages")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+public class ChattingMessage extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nickname;
+
+    private String content;
+
+    private String sessionId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "chatting_room_id")
+    private ChattingRoom chattingRoom;
+
+
+}
