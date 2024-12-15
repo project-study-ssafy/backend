9b9a9aae7ad0c1e3db57c79a15b5ff71107f8637
jongchan0109 <jongchan1009@naver.com>
2024-12-15 14:47:22 +0900

    feat: 예외 코드 구현 #58

--- src/main/java/com/ssafeople/backend/global/response/failure/ErrorCode.java
+++ src/main/java/com/ssafeople/backend/global/response/failure/ErrorCode.java
@@ -29,7 +29,9 @@ public enum ErrorCode {
     INVALID_POST_ID(404, "현재 접근하려는 게시글이 존재하지 않습니다."),
     EMPTY_POST_LIST(400, "게시글 목록이 존재하지 않음"),
 
-    OWNER_NOTEQUAL_NOW(403, "해당 게시글에 대한 권한이 없습니다.");
+    OWNER_NOTEQUAL_NOW(403, "해당 게시글에 대한 권한이 없습니다."),
+    NOT_EXIST_CHATTING_ROOM(404, "해당 채팅방이 존재하지 않습니다."),
+    USER_NOT_LOGGED_IN(401, "사용자가 로그인하지 않았습니다.");
 
     private final int status;
     private final String reason;
