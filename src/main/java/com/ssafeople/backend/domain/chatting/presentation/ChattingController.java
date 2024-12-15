ca21055e38f3f67b65ea930695116d397baf7c47
jongchan0109 <jongchan1009@naver.com>
2024-12-15 14:44:50 +0900

    feat: 메세지 전송 API 구현 #58

---
+++ src/main/java/com/ssafeople/backend/domain/chatting/presentation/ChattingController.java
@@ -0,0 +1,45 @@
+package com.ssafeople.backend.domain.chatting.presentation;
+
+import com.ssafeople.backend.domain.chatting.presentation.dto.request.ChattingRequest;
+import com.ssafeople.backend.domain.chatting.presentation.dto.response.ChattingResponse;
+import com.ssafeople.backend.domain.chatting.service.ChattingService;
+import com.ssafeople.backend.domain.user.domain.User;
+import com.ssafeople.backend.global.utils.user.UserUtils;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+public class ChattingController {
+
+    private final SimpMessagingTemplate template;
+    private final ChattingService chattingService;
+    private final UserUtils userUtils;
+
+    @MessageMapping("/send/{roomId}")
+    public void sendToRoom(@RequestBody ChattingRequest chatMessageDto, @DestinationVariable Short roomId, Message<?> message) {
+        log.info("Room {} Message: {}", roomId, chatMessageDto.getContent());
+        StompHeaderAccessor accessor = StompHeaderAccessor.wrap(message);
+        String simpSessionId = accessor.getSessionId();
+        User user = null;
+
+        if (roomId == 2) {
+            user = userUtils.getCurrentUser();
+        }
+
+        List<ChattingResponse> responses = chattingService.sendMessage(
+            roomId, chatMessageDto.getContent(), user, simpSessionId);
+
+        template.convertAndSend("/sub/chat/room/" + roomId, responses);
+    }
+}
