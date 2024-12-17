711e5f154608bfc1db74c6393461e2f88ffa391b
jongchan0109 <jongchan1009@naver.com>
2024-12-17 17:03:23 +0900

    feat: 게시판 작성 기능 구현 #79

--- src/main/java/com/ssafeople/backend/domain/board/service/BoardServiceImpl.java
+++ src/main/java/com/ssafeople/backend/domain/board/service/BoardServiceImpl.java
@@ -35,5 +35,10 @@ public class BoardServiceImpl implements BoardService {
         return boardRepository.findById(id).orElseThrow(() -> BoardNotInRepositoryException.EXCEPTION);
     }
 
+    @Override
+    public void writeBoard(String boardName, String description) {
+        Board board = new Board(boardName, description);
+        boardRepository.save(board);
+    }
 
 }
