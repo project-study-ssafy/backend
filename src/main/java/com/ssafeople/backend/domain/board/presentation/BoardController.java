a54b72aecb051e1fcb625c569a3ca29fda40cd0d
name <grandsword25@gmail.com>
2024-12-17 15:07:44 +0900

    fix: 게시판 컨트롤러 수정 #26

--- src/main/java/com/ssafeople/backend/domain/board/presentation/BoardController.java
+++ src/main/java/com/ssafeople/backend/domain/board/presentation/BoardController.java
@@ -5,12 +5,9 @@ import com.ssafeople.backend.domain.board.service.BoardService;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.List;
 
 @Slf4j
 @RestController
@@ -21,15 +18,14 @@ public class BoardController {
     private final BoardService boardService;
 
     @GetMapping
-    public ResponseEntity<Map<String, Object>> home(@AuthenticationPrincipal String email) {
-        Map<String, Object> map = new HashMap<>();
-        return ResponseEntity.ok(map);
+    public List<Board> getBoardsList() {
+        return boardService.getAllBoards();
     }
 
     @GetMapping("/{id}")
     @Operation(summary = "게시판 조회", description = "특정 게시판을 조회하는 API")
-    public ResponseEntity<Board> getBoardById(@PathVariable Short id) {
-        return ResponseEntity.ok(boardService.getBoardById(id));
+    public Board getBoardById(@PathVariable Short id) {
+        return boardService.getBoardById(id);
     }
 
 }
