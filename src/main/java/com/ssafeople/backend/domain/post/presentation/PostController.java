e0cc9f7dbb19f380e01feaad511ac0ede55d9127
name <grandsword25@gmail.com>
2024-12-18 14:46:25 +0900

    feat: 좋아요 API 구현 #75

--- src/main/java/com/ssafeople/backend/domain/post/presentation/PostController.java
+++ src/main/java/com/ssafeople/backend/domain/post/presentation/PostController.java
@@ -1,5 +1,6 @@
 package com.ssafeople.backend.domain.post.presentation;
 
+import com.ssafeople.backend.domain.like.service.LikeService;
 import com.ssafeople.backend.domain.post.presentation.dto.request.PostUpdateRequest;
 import com.ssafeople.backend.domain.post.presentation.dto.request.PostWriteRequest;
 import com.ssafeople.backend.domain.post.presentation.dto.response.PostDetailResponse;
@@ -21,6 +22,7 @@ import java.util.*;
 public class PostController {
 
     private final PostService postService;
+    private final LikeService likeService;
     private final UserUtils userUtils;
 
     @GetMapping
@@ -69,5 +71,23 @@ public class PostController {
     ) {
         User user = userUtils.getCurrentUser();
         postService.deletePost(postId, user);
+    }
+
+    @PostMapping("/{postId}/like")
+    public void doLikePost(
+            @PathVariable(name = "postId") Long postId
+    ) {
+        User user = userUtils.getCurrentUser();
+
+        likeService.doLikePost(user, postId);
+    }
+
+    @PostMapping("/{postId}/unlike")
+    public void doUnLikePost(
+            @PathVariable(name = "postId") Long postId
+    ) {
+        User user = userUtils.getCurrentUser();
+
+        likeService.undoLikePost(user, postId);
     }
 }
